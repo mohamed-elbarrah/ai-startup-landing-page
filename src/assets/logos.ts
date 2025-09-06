671fddeedcdd9bcbc92b73918812239d7d46375a
mohamed-elbarrah <elbarrahsimo@gmail.com>
2025-09-06 15:47:34 -0700

    add logo tricker

---
+++ src/assets/logos.ts
@@ -0,0 +1,15 @@
+import acmeLogo from "./logo-acme.png";
+import apexLogo from "./logo-apex.png";
+import celestialLogo from "./logo-celestial.png";
+import quantumLogo from "./logo-quantum.png";
+import pulseLogo from "./logo-pulse.png";
+import echoLogo from "./logo-echo.png";
+
+export const logos = [
+  acmeLogo,
+  apexLogo,
+  celestialLogo,
+  quantumLogo,
+  pulseLogo,
+  echoLogo,
+];
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,8 +4,9 @@ import Button from "@/components/Button";
 export const Header = () => {
   return (
     <header className="p-4 border-b border-white/15 md:border-none sticky top-0 z-50">
+      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
       <div className="md:container">
-        <div className="flex justify-between items-center md:border border-white/15 rounded-xl p-2.5 max-w-2xl mx-auto backdrop-blur ">
+        <div className="flex justify-between items-center md:border border-white/15 rounded-xl p-2.5 max-w-2xl mx-auto md:backdrop-blur ">
           <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
             <LogoIcon className="w-8 h-8 cursor-pointer" />
           </div>
